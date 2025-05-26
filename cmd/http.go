184cf3be0de7992ccd7568f8343ef8474b832035
Bima Setya Nugraha <setyabima016@gmail.com>
2025-05-26 09:16:19 +0700

    add Fitur Create Wallet

--- cmd/http.go
+++ cmd/http.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"ewallet-wallet/helpers"
 	"ewallet-wallet/internal/api"
+	"ewallet-wallet/internal/interfaces"
+	"ewallet-wallet/internal/repository"
 	"ewallet-wallet/internal/services"
 	"log"
 
@@ -10,6 +12,7 @@ import (
 )
 
 func ServeHTTP() {
+	dependency := dependencyInject()
 	healthCheckSVC := &services.HealthCheck{}
 	healtCheckAPI := &api.HealthCheck{
 		HealthCheckServices: healthCheckSVC,
@@ -17,8 +20,42 @@ func ServeHTTP() {
 
 	r := gin.Default()
 	r.GET("/health", healtCheckAPI.HealthChecHandlerHTTP)
-	err := r.Run(":" + helpers.GetEnv("PORT", "8080"))
+
+	walletV1 := r.Group("/wallet/v1")
+	walletV1.POST("/", dependency.WalletApi.Create)
+
+	err := r.Run(":" + helpers.GetEnv("PORT", "8081"))
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type Dependency struct {
+	HealtyCheckApi   interfaces.IHealthCheckApi
+	WalletRepository interfaces.IWalletRepossitory
+	WalletApi        interfaces.IWalletApi
+}
+
+func dependencyInject() Dependency {
+	healtyCheckSVC := &services.HealthCheck{}
+	healtyCheckAPI := &api.HealthCheck{
+		HealthCheckServices: healtyCheckSVC,
+	}
+
+	walletRepository := &repository.WalletRepository{
+		DB: helpers.DB,
+	}
+	walletSVC := &services.WalletService{
+		WalletRepository: walletRepository,
+	}
+	walletAPI := &api.WalletApi{
+		WalletService: walletSVC,
+	}
+
+	return Dependency{
+		HealtyCheckApi:   healtyCheckAPI,
+		WalletRepository: walletRepository,
+		WalletApi:        walletAPI,
+	}
+
 }
